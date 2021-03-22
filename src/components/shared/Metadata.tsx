656386430c61cbf80580e8c39a0294857bec0fe3
luxumbra <me@luxumbra.dev>
2021-03-22 13:44:16 +0000

    Adding og tags/image and favicon, etc.

--- src/components/shared/Metadata.tsx
+++ src/components/shared/Metadata.tsx
@@ -18,13 +18,23 @@ export const MetadataComponent: FC<MetadataInterface> = ({ title, description })
             <meta name="twitter:card" content="summary"/>
             <meta name="twitter:title" content={outputTitle}/>
             <meta name="twitter:description" content={outputDescription}/>
-
             <meta property="og:title" content={outputTitle}/>
-            <meta property="og:image" content=""/>
-            <meta property="og:description" content={outputDescription}/>
+            <meta property="og:image" content="/assets/lambo.png"/>
+            <meta property="og:description" content={outputDescription} />
+
+            <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png"/>
+            <link rel="manifest" href="/assets/site.webmanifest"/>
+            <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#ffaaff"/>
+            <link rel="shortcut icon" href="/assets/favicon.ico"/>
+            <meta name="msapplication-TileColor" content="#bbfcff"/>
+            <meta name="msapplication-config" content="/assets/browserconfig.xml"/>
+            <meta name="theme-color" content="#bbfcff"/>
+
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Asset&display=swap" rel="stylesheet"/>
-            <link rel="icon" type="image/jpg" href=""/>
+
         </Head>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { Box, Link, Heading } from '@chakra-ui/react';
 import { MetadataComponent } from '../components/shared/Metadata';
 import { AssetContainer } from '../components/shared/AssetContainer';

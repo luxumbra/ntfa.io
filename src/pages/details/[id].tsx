32f9e5a5ece2a02e5260a33a9804e623b85dd63f
luxumbra <me@luxumbra.dev>
2021-07-17 04:00:56 +0100

    WIP: Metadata & scrolling content on detail page.

--- src/pages/details/[id].tsx
+++ src/pages/details/[id].tsx
@@ -14,7 +14,6 @@ import { FooterComponent } from "../../components/shared/Footer";
 import { rootCertificates } from "node:tls";
 
 export let getAsset: any;
-
 export interface AssetDetailsInterface {
     name: string;
     description: string;
@@ -22,7 +21,7 @@ export interface AssetDetailsInterface {
     image_preview_url: string;
 }
 
-export const AssetDetails = () => {
+export function AssetDetails() {
     const [toggle1, setToggle1] = useState(false);
     const [toggle2, setToggle2] = useState(false);
     const [toggle3, setToggle3] = useState(false);
@@ -54,6 +53,54 @@ export const AssetDetails = () => {
             .catch((err) => console.error(err));
     }, [id, token]);
 
+    interface AssetMetaInterface {
+        theAsset: Array<{}>;
+    }
+    const AssetMeta: FC<AssetMetaInterface> = ({ theAsset }) => {
+        console.log('theAsset:', theAsset);
+        const metaItems = [] as Array<{}>;
+
+        theAsset?.traits?.map((trait: {}) => {
+            metaItems.push(trait);
+        });
+
+        return (
+            <Box as="ul" d="flex" w="100%" minW="100%" listStyleType="none" flexFlow="row wrap">
+                {metaItems.map((item, index) => {
+                    console.log('item: ', item);
+                    type ItemType = {
+                        trait_type: string;
+                        value: string;
+                    }
+                    const assetItem = item as ItemType;
+                    return (
+                        <Box as="li" sx={{
+                            flex: "0 0 45%",
+                            d: "flex",
+                            flexFlow: "row wrap",
+                            mb: 1,
+                        }}>
+                            <Box as="span" key={`dt-${index}`} sx={{
+                                flex: "0 0 100%",
+                                fontSize: { base: "14px" },
+                                fontWeight: "800",
+                            }}>
+                                {assetItem?.trait_type}
+                            </Box>
+                            <Box as="span" key={`dd-${index}`} sx={{
+                                flex: "1",
+                                fontSize: { base: "14px" },
+                                fontWeight: "100",
+                            }}>
+                                {assetItem?.value}
+                            </Box>
+                        </Box>
+                    );
+                }
+                )}
+            </Box>
+        )
+    }
 
     return (
         <Box
@@ -71,6 +118,7 @@ export const AssetDetails = () => {
                 position="relative"
                 flex={{ base: "0 0 90%", smd: "0 0 98%", lg: "0 0 33%" }}
                 width={{ base: "90%", smd: "98%", lg: "33%" }}
+                maxH="75vh"
                 d="flex"
                 flexDirection={{ base: "column", smd: "row", lg: "column" }}
                 alignItems={{ base: "flex-start", smd: "center", lg: "flex-start" }}
@@ -125,24 +173,38 @@ export const AssetDetails = () => {
                             margin="0"
                             // backgroundColor="rgba(255,255,255,0.6)"
                             // backdropFilter="blur(7px)"
+                            height={{
+                                base: `${100 * (356 / 633)}%`,
+                                smd: `${100 - 31}%`,
+                                lg: `${100 - ((356 / 633) * 100)}%`
+                            }}
+                            overflowY="auto"
                             z={0}
+                            sx={{
+                                scrollbarColor: "red",
+                                scrollbarGutter: "red"
+                            }}
                         >
-                            <Box p={{ base: "15px", smd: "10px", lg: "25px" }}>
+                            <Box p={{ base: "15px", smd: "10px", lg: "25px" }} d="flex" flexFlow="column wrap" >
                                 <Heading as="h3" size={"sm"} color="accent.primary" mb="2">
                                     {asset && asset.name}
                                 </Heading>
-                                <Box d="flex" flexFlow="column wrap" mb="4" sx={{
+                                <Box d="flex" className="asset--meta" flexFlow="column wrap" mb="4" sx={{
                                     "& > span": {
                                         fontSize: { base: "10px", lg: "12px" },
                                         mb: 0,
                                         fontWeight: "100"
                                     }
                                 }}>
-                                    {/* <span>{asset && goldVids[+id].NFT}</span>
-                            <span>{asset && goldVids[+id].vault}</span> */}
+
+                                    {asset && <AssetMeta theAsset={asset} />}
                                 </Box>
-                                <Box fontSize={{ base: "12px", lg: "14px" }}>
-                                    {asset?.description.replace('<br />', '\n')}
+                                <Box className="asset--description" fontSize={{ base: "12px", lg: "14px" }} sx={{
+                                    flex: "0 0 33%",
+                                    maxH: "33%",
+                                    width: "100%"
+                                }}>
+                                    {asset?.description.replace('<br />', ' \n\n')}
                                 </Box>
                             </Box>
                         </Box>
@@ -256,3 +318,27 @@ export const AssetDetails = () => {
 }
 
 export default AssetDetails;
+
+// AssetDetails.getInitialProps = async () => {
+//     const [loading, setLoading] = useState(true);
+//     const [asset, setAsset] = useState({} as AssetDetailsInterface);
+//     const { id, token } = router.query;
+
+//     getAsset = axios
+//         .get(
+//             `https://rinkeby-api.opensea.io/api/v1/asset/${id}/${token}`
+//         )
+//         .then((response) => {
+//             console.log('response: ', response);
+//             // debugger;
+//             // setGoldAssets(response.data.assets.slice(0, 2));
+//             // setAssets(response.data.assets.slice(3));
+//             setAsset(response.data);
+//         })
+//         .then(() => {
+//             setLoading(false);
+//         })
+//         .catch((err) => console.error(err));
+
+//     return asset;
+// }

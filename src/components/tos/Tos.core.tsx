946a6a87647216bec6c9473ef559296b05422d12
luxumbra <me@luxumbra.dev>
2021-07-17 15:31:14 +0100

    tos height

--- src/components/tos/Tos.core.tsx
+++ src/components/tos/Tos.core.tsx
@@ -16,7 +16,7 @@ function TosCore({ content }: any) {
 
             {/* <AboutSceneLeft /> */}
 
-            <Box className="scene__center" flex={{ base: "0 0 90%", smd: "0 0 50%", md: "0 0 66%", xxxl: "0 0 66%" }} ml={["auto", "auto"]} mr={{ base: "auto" }} d="flex" flexDirection="column" alignItems="center" maxH="95vh" zIndex="1000">
+            <Box className="scene__center" flex={{ base: "0 0 90%", smd: "0 0 50%", md: "0 0 66%", xxxl: "0 0 66%" }} ml={["auto", "auto"]} mr={{ base: "auto" }} d="flex" flexDirection="column" alignItems="center" maxH={{ base: "80vh", lg: "95vh" }} zIndex="1000">
                 <Box width="100%" height="auto" margin={{ base: "50px 0 0", smd: "25px 0 0", lg: "50px 0 0" }} zIndex={1000} maxH={{ base: "90%", smd: "85%", md: "100%" }} py="50px" overflow="hidden" borderRadius="6px" boxShadow="0 0 15px rgba(0,0,0,0.5)" backgroundColor="rgba(0,0,0,0.8)" position="relative">
                     <Heading as="h2" fontSize={{ base: `9px`, lg: "14px", xl: `16px`, xxxl: `18px` }} sx={{
                         color: `white`,

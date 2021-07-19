69ae1880cfa12f56aae3ca0662e52f797f7b8856
luxumbra <me@luxumbra.dev>
2021-07-19 16:21:36 +0100

    WIP

---
+++ config.ts
@@ -0,0 +1,6 @@
+export const CONFIG = {
+  infuraId:
+    process.env.NEXT_PUBLIC_INFURA_ID || '781d8466252d47508e177b8637b1c2fd',
+  openseaApiKey: process.env.NEXT_OPENSEA_API_KEY || undefined,
+  publicUrl: process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000',
+};
